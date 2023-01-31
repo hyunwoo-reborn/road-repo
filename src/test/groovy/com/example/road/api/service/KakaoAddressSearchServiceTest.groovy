858fdcd13a3303f9516c9e74336d92ce9b00f7e8
hyuwnoo.park.reborn <hyunnwoo.park.reborn@gmail.com>
2023-02-01 02:02:39 +0900

    commit

---
+++ src/test/groovy/com/example/road/api/service/KakaoAddressSearchServiceTest.groovy
@@ -0,0 +1,36 @@
+package com.example.road.api.service
+
+import com.example.road.AbstractIntegrationContainerBaseTest
+import com.example.road.api.dto.KakaoApiResponseDto
+import org.springframework.beans.factory.annotation.Autowired
+
+class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private KakaoAddressSearchService kakaoAddressSearchService
+
+    def "address 파라미터 값이 null이면, requestAddressSearch 메소드는 null을 리턴한다."() {
+        given:
+        String address = null
+
+        when:
+        def result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result == null
+
+    }
+
+    def "주소값이 valid한다면, KakaoAddressSearch 메소드는 정상적으로 document를 반환한다."() {
+        given:
+        String address = "서울 성북구 종암로 10길"
+
+        when:
+        KakaoApiResponseDto result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result.documentList.size() > 0
+        result.metaDto.totalCount > 0
+        result.documentList.get(0).addressName != null
+    }
+}
