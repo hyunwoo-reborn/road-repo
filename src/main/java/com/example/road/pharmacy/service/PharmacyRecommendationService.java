23db3ebac19d93d3b0cedfe42211d0c73344665f
hyunwoo.park <hyunwoo.park.reborn@gmail.com>
2023-02-14 00:28:55 +0900

    MockMVC 테스트 케이스 작성

--- src/main/java/com/example/road/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/road/pharmacy/service/PharmacyRecommendationService.java
@@ -50,7 +50,7 @@ public class PharmacyRecommendationService {
 
     private OutputDto convertToOutputDto(Direction direction) {
         return OutputDto.builder()
-                .pharmacyAddress(direction.getTargetPharmacyName())
+                .pharmacyName(direction.getTargetPharmacyName())
                 .pharmacyAddress(direction.getTargetAddress())
                 .directionUrl("todo")
                 .roadViewUrl("todo")
